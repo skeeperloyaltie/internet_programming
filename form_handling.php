8e7671a4ec6be11bcf2045c6aac0e353a139cf4e
skeeperloyaltie <gugod254@gmail.com>
2023-06-30 19:42:22 +0300

    aaargh

---
+++ form_handling.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    
+    </head>
+    <body>
+    <!--A form that has two inputs -->
+    <form action="" method="POST">
+    First Name: <input type="text" id="First Name" name="first_name" placeholder="John"> <br /> Last Name: <input type="text" id="Last Name" placeholder="Doe"><br />
+        Submission: <input type="date" id="date" name="date" placeholder="Today's Date">
+
+        <input type="submit" value="Submit">
+        <?php 
+            // A form that has two inputs 
+            
+        ?>
+    </form>
+    </body>
+</html>
