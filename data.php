b61a6ec4613d3c5f7c5bd27ab43bf45bc86a5815
skeeperloyaltie <gugod254@gmail.com>
2023-06-30 20:15:11 +0300

    fixed, error reporting()

---
+++ data.php
@@ -0,0 +1,6 @@
+<?php 
+            // A form that has two inputs 
+            $fname = $_POST['first_name'];
+            $lname = $_POST['lname'];
+            echo "Hello, ". $fname. ", your last name is :  " .$lname;
+        ?>
--- form_handling.php
+++ form_handling.php
@@ -4,16 +4,15 @@
     
     </head>
     <body>
+    <?php 
+    error_reporting(E_ALL);
+    ?>
     <!--A form that has two inputs -->
-    <form action="" method="POST">
-    First Name: <input type="text" id="First Name" name="first_name" placeholder="John"> <br /> Last Name: <input type="text" id="Last Name" placeholder="Doe"><br />
+    <form action="data.php" method="POST">
+    First Name: <input type="text" id="First Name" name="first_name" placeholder="John"> <br /> Last Name: <input type="text" id="Last Name" name="lname" placeholder="Doe"><br />
         Submission: <input type="date" id="date" name="date" placeholder="Today's Date">
 
         <input type="submit" value="Submit">
-        <?php 
-            // A form that has two inputs 
-            
-        ?>
     </form>
     </body>
 </html>
