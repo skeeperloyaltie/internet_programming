db1df7dcece78f01f901d809587d25cfa6c8254b
skeeperloyaltie <gugod254@gmail.com>
2023-06-30 19:20:36 +0300

    ommited, function my error custom

--- functions.php
+++ functions.php
@@ -8,4 +8,17 @@
 
     $c = 10;
     echo "The total is ".sum(5,$c);
+
+    function myerror($errno, $errmsg)
+    {
+        echo "An error occured, ". $errno . $errmsg .  " <br/>";
+        die;
+    };
+
+    $errno = 5;
+    $errmsg = "Check the values";
+    if (sum(5, $c) < 16)
+    {
+        myerror($errno, $errmsg);
+    };
 ?>
